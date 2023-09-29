4a7bef57e230100c09d35ff052342ae8f0b17e0d
Nitish kumar <nitish.bpns@gmail.com>
2023-09-29 15:45:39 +0530

    test

---
+++ src/components/Testing.js
@@ -0,0 +1,42 @@
+// import logo from './logo.png';
+import "./App.css";
+import React, { useState } from "react";
+import QrReader from "react-web-qr-reader";
+
+function Testing() {
+  const delay = 500;
+
+  const previewStyle = {
+    height: 240,
+    width: 320,
+  };
+
+  const [result, setResult] = useState("No result");
+
+  const handleScan = (result) => {
+    if (result) {
+      setResult(result);
+    }
+  };
+
+  const handleError = (error) => {
+    console.log(error);
+  };
+
+  return (
+    <div className="App">
+      <center>
+        <QrReader
+          facingMode="environment"
+          delay={delay}
+          style={previewStyle}
+          onError={handleError}
+          onScan={handleScan}
+        />
+        <p>{result}</p>
+      </center>
+    </div>
+  );
+}
+
+export default Testing;
